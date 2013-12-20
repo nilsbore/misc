e444a8298614099794ee353fa379d39d2ebbce79
Rares Ambrus <raambrus@kth.se>
2013-12-20 10:04:53 +0100

    Corrected name of the subscribed point cloud topic

--- disjoint_snapshots/src/disjoint_snapshots_node.cpp
+++ disjoint_snapshots/src/disjoint_snapshots_node.cpp
@@ -17,11 +17,11 @@ int main(int argc, char** argv)
     disjoint_snapshots d(n, client, folder, 2.0);
 
 	typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> MySyncPolicy;
-    message_filters::Subscriber<sensor_msgs::Image> depth_sub(n, "/head_xtion/depth/image_raw", 1);
-    message_filters::Subscriber<sensor_msgs::Image> rgb_sub(n, "/head_xtion/rgb/image_color", 1);
+    message_filters::Subscriber<sensor_msgs::Image> depth_sub(n, "/chest_xtion/depth/image_raw", 1);
+    message_filters::Subscriber<sensor_msgs::Image> rgb_sub(n, "/chest_xtion/rgb/image_color", 1);
     message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), depth_sub, rgb_sub);
     sync.registerCallback(&disjoint_snapshots::image_callback, &d);
-	ros::Subscriber pcd_sub = n.subscribe("/head_xtion/registered/points", 1, &disjoint_snapshots::pointcloud_callback, &d);
+	ros::Subscriber pcd_sub = n.subscribe("/chest_xtion/depth_registered/points", 1, &disjoint_snapshots::pointcloud_callback, &d);
 	ros::Subscriber sub = n.subscribe("/odom", 1, &disjoint_snapshots::geometry_callback, &d);
 
 	std::cout << "Spinning..." << std::endl;
